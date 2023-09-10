b2a4208093ec507a0b7dc9efb5164012ad82b5fe
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-09-10 18:02:29 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// GicellyGomes
+
+string nome,nome2, auxiliar;
+
+Console.WriteLine("---Troca de Variaveis---\n");
+
+Console.Write("nome..: Tom Riddle");
+nome = Console.ReadLine()!;
+
+Console.Write("nome2..:Lord Voldemort");
+nome2 = Console.ReadLine()!;
+
+auxiliar = nome;
+nome = nome2;
+nome2 = auxiliar;
+
+
+Console.WriteLine("\nApós Trocado os variaveis:\n");
+Console.WriteLine($"Tom Riddle = {nome}");
+Console.WriteLine($"Lord Voldemort = {nome2}");
